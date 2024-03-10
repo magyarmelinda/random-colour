f4b5ef7f3b7c3128f611ff723ea4904ffa0e5211
メリンダ <m4gy4r.m3l1nd4@gmail.com>
2024-03-10 06:02:15 +0000

    refactor: follow conventions

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const h1 = document.querySelector('h1');
 const button = document.querySelector('button');
 
 function randomColor() {
-    let R = Math.floor(Math.random() * 255);
-    let G = Math.floor(Math.random() * 255);
-    let B = Math.floor(Math.random() * 255);
+  let R = Math.floor(Math.random() * 255);
+  let G = Math.floor(Math.random() * 255);
+  let B = Math.floor(Math.random() * 255);
 
-    return `rgb(${R}, ${G}, ${B})`;
+  return `rgb(${R}, ${G}, ${B})`;
 }
 
-button.addEventListener("click", () => {
-    const color = randomColor();
-    h1.innerText = color;
-    document.body.style.backgroundColor = color;
+button.addEventListener('click', () => {
+  const color = randomColor();
+  h1.innerText = color;
+  document.body.style.backgroundColor = color;
 });
