bcf89b7006ffbd3ed06ee3bf0b328eeb17d19197
Magyar Melinda Barbara <m4gy4r.m3l1nd4@gmail.com>
2024-10-11 03:42:27 +0000

    chore: reformat

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const h1 = document.querySelector('h1');
-const button = document.querySelector('button');
+const h1 = document.querySelector("h1");
+const button = document.querySelector("button");
 
 function randomColor() {
   let R = Math.floor(Math.random() * 255);
@@ -9,7 +9,7 @@ function randomColor() {
   return `rgb(${R}, ${G}, ${B})`;
 }
 
-button.addEventListener('click', () => {
+button.addEventListener("click", () => {
   const color = randomColor();
   h1.innerText = color;
   document.body.style.backgroundColor = color;
